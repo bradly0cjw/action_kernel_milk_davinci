46f9ac3e7fb8970a886fb2990c48a2f1d1fccf7d
Bradly Chang <57744542+bradly0cjw@users.noreply.github.com>
2024-06-30 14:15:16 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -12,7 +12,7 @@ KERNEL_TYPE="vantom"
 # KernelSU
 KERNELSU_REPO="tiann/KernelSU"
 KSU_ENABLED="true"
-KSU_TARGET="v0.9.5"
+KSU_TARGET=""
 
 # Anykernel3
 ANYKERNEL3_GIT="https://github.com/SchweGELBin/AnyKernel3_davinci.git"
